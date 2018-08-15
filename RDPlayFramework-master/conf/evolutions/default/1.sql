1154c2814aa3bfb926f2811480e94462ea3c0f1b
LeTuan <tuanld@runsystem.net>
2018-08-15 10:29:16 +0700

    Update code chat member

--- RDPlayFramework-master/conf/evolutions/default/1.sql
+++ RDPlayFramework-master/conf/evolutions/default/1.sql
@@ -3,18 +3,39 @@
 
 # --- !Ups
 
-create table user (
-  id                            varchar(255) not null,
-  username                      varchar(255) not null,
-  password                      varchar(255) not null,
-  status                        varchar(1),
-  role                          varchar(10) not null,
-  avatar                        varchar(255),
-  constraint pk_user primary key (id)
+create table groups (
+  id                            varchar2(255) not null,
+  groupsname                    varchar2(255) not null,
+  status                        varchar2(1) not null,
+  description                   varchar2(255),
+  constraint pk_groups primary key (id)
+);
+
+create table member (
+  id                            varchar2(255) not null,
+  username                      varchar2(20) not null,
+  password                      varchar2(20) not null,
+  status                        varchar2(2),
+  role                          varchar2(20) not null,
+  avatar                        varchar2(255),
+  description                   varchar2(255),
+  constraint pk_member primary key (id)
+);
+
+create table members_group (
+  id                            varchar2(255),
+  group_id                      varchar2(255),
+  member_id                     varchar2(255),
+  status                        varchar2(255),
+  username                      varchar2(255)
 );
 
 
 # --- !Downs
 
-drop table if exists user;
+drop table groups cascade constraints purge;
+
+drop table member cascade constraints purge;
+
+drop table members_group cascade constraints purge;
 

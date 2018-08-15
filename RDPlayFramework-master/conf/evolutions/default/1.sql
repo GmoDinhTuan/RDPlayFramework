3e233a716ae316007b6838002b9f86a729b7931b
hungtt1 <hungtt1@runsystem.net>
2018-08-15 13:58:28 +0700

    no message

--- RDPlayFramework-master/conf/evolutions/default/1.sql
+++ RDPlayFramework-master/conf/evolutions/default/1.sql
@@ -3,18 +3,45 @@
 
 # --- !Ups
 
-create table user (
-  id                            varchar(255) not null,
-  username                      varchar(255) not null,
-  password                      varchar(255) not null,
-  status                        varchar(1),
-  role                          varchar(10) not null,
-  avatar                        varchar(255),
-  constraint pk_user primary key (id)
+create table groups (
+  id                            number(19) not null,
+  groupsname                    varchar2(255) not null,
+  status                        varchar2(1) not null,
+  description                   varchar2(255),
+  constraint pk_groups primary key (id)
 );
+create sequence GROUPS_SEQ increment by 1;
+
+create table member (
+  id                            number(19) not null,
+  username                      varchar2(20) not null,
+  password                      varchar2(20) not null,
+  status                        varchar2(2),
+  role                          varchar2(20) not null,
+  avatar                        varchar2(255),
+  description                   varchar2(255),
+  constraint pk_member primary key (id)
+);
+create sequence MEMBER_SEQ increment by 1;
+
+create table membersgroup (
+  id                            number(19) not null,
+  groupid                       number(19) not null,
+  memberid                      number(19) not null,
+  status                        varchar2(255) not null,
+  constraint pk_membersgroup primary key (id)
+);
+create sequence MEMBERSGROUP_SEQ increment by 1;
 
 
 # --- !Downs
 
-drop table if exists user;
+drop table groups cascade constraints purge;
+drop sequence GROUPS_SEQ;
+
+drop table member cascade constraints purge;
+drop sequence MEMBER_SEQ;
+
+drop table membersgroup cascade constraints purge;
+drop sequence MEMBERSGROUP_SEQ;
 
